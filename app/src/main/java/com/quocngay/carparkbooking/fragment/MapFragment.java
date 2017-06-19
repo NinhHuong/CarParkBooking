a29d4da17e7fcb1a9b5c61aee450119804abc839
Hoàng Đức Long <nothing.noonedr@gmail.com>
2017-06-19 22:38:45 +0700

    Add fragment map. Need to add search bar, your location.

--- app/src/main/java/com/quocngay/carparkbooking/fragment/MapFragment.java
+++ app/src/main/java/com/quocngay/carparkbooking/fragment/MapFragment.java
@@ -1,21 +1,39 @@
 package com.quocngay.carparkbooking.fragment;
 
-import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.quocngay.carparkbooking.R;
 
 import com.quocngay.carparkbooking.R;
 
-public class MapFragment extends Fragment  {
-    public MapFragment() {
-        // Required empty public constructor
-    }
+public class MapFragment extends Fragment implements View.OnClickListener {
+    MapView mMapView;
+    private GoogleMap googleMap;
+
+    TextView txtName;
+    EditText edtSearchBar;
+    Button btnSearch;
+
+//    public MapFragment() {
+//        // Required empty public constructor
+//    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -23,11 +41,89 @@ public class MapFragment extends Fragment  {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_map, container, false);
+
+        edtSearchBar =(EditText) rootView.findViewById(R.id.edtSearchBar);
+        btnSearch =(Button) rootView.findViewById(R.id.btnSearch);
+        btnSearch.setOnClickListener(this);
+
+        mMapView = (MapView) rootView.findViewById(R.id.mapView);
+        mMapView.onCreate(savedInstanceState);
+
+        mMapView.onResume(); // needed to get the map to display immediately
+
+        try {
+            MapsInitializer.initialize(getActivity().getApplicationContext());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        mMapView.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(GoogleMap mMap) {
+                googleMap = mMap;
+
+                // For showing a move to my location button
+                googleMap.setMyLocationEnabled(true);
+                googleMap.getUiSettings().setMyLocationButtonEnabled(true);
+
+                // For dropping a marker at a point on the Map
+                LatLng sydney = new LatLng(21.027807, 105.778542);
+                googleMap.addMarker(new MarkerOptions().position(sydney).title("Marker Title").snippet("Marker Description"));
+
+                // For zooming automatically to the location of the marker
+                CameraPosition cameraPosition = new CameraPosition.Builder().target(sydney).zoom(12).build();
+                googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+            }
+        });
 
 
-        return inflater.inflate(R.layout.fragment_map, container, false);
+
+
+        return rootView;
+    }
+
+
+
+    //region Override normal function
+    @Override
+    public void onResume() {
+        super.onResume();
+        mMapView.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mMapView.onPause();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mMapView.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mMapView.onLowMemory();
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btnSearch:
+                Search();
+                break;
+            default:
+                return;
+        }
+    }
+    //endregion
+
+    private void Search(){
+        Toast.makeText(getContext(),"Start search " + edtSearchBar.getText().toString(),Toast.LENGTH_SHORT).show();
     }
 }
